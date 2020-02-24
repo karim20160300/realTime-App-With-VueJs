ca78b9d068e31a3fab807f6bb608fcd65d96600a
unknown <karimz19299@gmail.com>
2020-02-24 07:15:53 -0800

    CRUD For Reply along with ReplyResouce

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ReplyResource;
+use App\Model\Question;
 use App\Model\Reply;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ReplyController extends Controller
 {
@@ -12,9 +15,10 @@ class ReplyController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Question $question)
     {
-        //
+        // get all replies for spesific question
+        return ReplyResource::collection($question->replies);
     }
 
     /**
@@ -33,9 +37,11 @@ class ReplyController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Question $question, Request $request)
     {
-        //
+        //  
+        $reply = $question->replies()->create($request->all());
+        return response(['reply' => new ReplyResource($reply)], Response::HTTP_CREATED);
     }
 
     /**
@@ -44,9 +50,12 @@ class ReplyController extends Controller
      * @param  \App\Model\Reply  $reply
      * @return \Illuminate\Http\Response
      */
-    public function show(Reply $reply)
+    public function show(Question $question, Reply $reply)
     {
-        //
+        //return spesific reply for specific question
+
+        return new ReplyResource($reply);
+        
     }
 
     /**
@@ -67,9 +76,11 @@ class ReplyController extends Controller
      * @param  \App\Model\Reply  $reply
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Reply $reply)
+    public function update(Question $question, Request $request, Reply $reply)
     {
         //
+        $reply->update($request->all());
+        return response('Updated!', Response::HTTP_ACCEPTED)->header('Content-Type', 'text/plain');
     }
 
     /**
@@ -78,8 +89,10 @@ class ReplyController extends Controller
      * @param  \App\Model\Reply  $reply
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Reply $reply)
+    public function destroy(Question $question, Reply $reply)
     {
         //
+        $reply->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
