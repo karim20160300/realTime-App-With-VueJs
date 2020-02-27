0fa9921e2c4e89813867e5bfe1051c083bb9ee87
unknown <karimz19299@gmail.com>
2020-02-26 21:42:57 -0800

    Vuetify, VueRouter and InitialComponnets

---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,45 @@
+<template>
+<div id="app">
+  <v-app id="inspire">
+    <v-card
+      color="grey lighten-4"
+      flat
+      height="200px"
+      tile
+    >
+      <v-toolbar dense>
+       
+  
+        <v-toolbar-title>Fourm</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn text>
+          Fourm
+        </v-btn>
+  
+        <v-btn text>
+          Ask Questions
+        </v-btn>
+  
+        <v-btn text>
+         Categories
+        </v-btn>
+
+        <v-btn text>
+         LogIn
+        </v-btn>
+      </v-toolbar>
+    </v-card>
+  </v-app>
+</div>
+</template>
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
