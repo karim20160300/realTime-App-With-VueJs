29dd10b26fd09ca0f6c69b2ccc5615f4780e5d3a
unknown <karimz19299@gmail.com>
2020-02-28 10:37:21 -0800

    Login Componnets and login

--- resources/js/components/Toolbar.vue
+++ resources/js/components/Toolbar.vue
@@ -1,37 +1,21 @@
 <template>
 <div id="app">
-  <v-app id="inspire">
-    <v-card
-      color="grey lighten-4"
-      flat
-      height="200px"
-      tile
-    >
+    <v-card>
       <v-toolbar dense>
        
-  
+          
         <v-toolbar-title>Fourm</v-toolbar-title>
   
         <v-spacer></v-spacer>
   
-        <v-btn text>
-          Fourm
-        </v-btn>
-  
-        <v-btn text>
-          Ask Questions
-        </v-btn>
-  
-        <v-btn text>
-         Categories
-        </v-btn>
-
-        <v-btn text>
-         LogIn
-        </v-btn>
+        <div>
+        <router-link to="/fourm">Fourm</router-link>
+        <router-link to="/ask">Ask Questions</router-link>
+        <router-link to="/categories">Categories</router-link>
+        <router-link to="/login">LogIn</router-link>
+        </div>
       </v-toolbar>
     </v-card>
-  </v-app>
 </div>
 </template>
 <script>
